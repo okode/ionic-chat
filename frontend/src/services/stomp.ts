1ac5db4c36083376b49d76dc776b1514f100da58
Pedro Jorquera <pjorquera@okode.com>
2017-03-21 13:34:17 +0100

    Refactor

--- frontend/src/services/stomp.ts
+++ frontend/src/services/stomp.ts
@@ -14,27 +14,34 @@ export class StompService {
   private wsbaseuri = 'ws://localhost:8080';
   private listeners = new Map<string, { listener: Observable<Stomp.Message>, subscriber: Subscriber<Stomp.Message> }>();
 
+  private isReadyInProgress = false;
+  private readyPromiseResolved: PromiseResolve;
+  private readyPromise: Promise<void>;
+
   constructor() {
     if (!isDevMode()) {
       this.wsbaseuri = `${window.location.protocol.startsWith('https') ? 'wss' : 'ws'}://${window.location.host}`;
     }
     this.stomp = Stomp.client(`${this.wsbaseuri}/chat/websocket`);
-    this.connect();
+    this.ready();
   }
 
-  private connect(resolve?: PromiseResolve) {
+  private connect() {
     this.stomp.connect({ login: '', passcode: '' },
       (frame?: Stomp.Frame) => {
         this.listeners.forEach((listener, topic) => {
           this.stomp.subscribe(topic, message => { listener.subscriber.next(message); });
         });
-        if (resolve != null) resolve();
+        if (this.readyPromiseResolved != null) {
+          this.readyPromiseResolved();
+        }
+        this.isReadyInProgress = false;
       },
       (error: string) => {
         setTimeout(() => {
           console.log('Stomp Reconnecting...');
           this.stomp = Stomp.client(`${this.wsbaseuri}/chat/websocket`);
-          this.connect(resolve);
+          this.connect();
         }, 5000);
       }
     );
@@ -42,15 +49,22 @@ export class StompService {
 
   private ready() {
     if (this.stomp.connected) return Promise.resolve();
-    return new Promise<void>(resolve => this.connect(resolve));
+    if (!this.isReadyInProgress) {
+      this.isReadyInProgress = true;
+      this.readyPromise = new Promise<void>((resolve, reject) => {
+        this.readyPromiseResolved = resolve;
+      });
+      this.connect();
+    }
+    return this.readyPromise;
   }
 
   listen(topic: string) {
     let listener = new Observable<StompMessage>((subscriber: Subscriber<StompMessage>) => {
       this.ready().then(() => {
-        this.stomp.subscribe(topic, message => { subscriber.next(message);
         this.listeners.set(topic, { listener: listener, subscriber: subscriber });
-      })});
+        this.stomp.subscribe(topic, message => { subscriber.next(message); });
+      });
     });
     return listener;
   }
